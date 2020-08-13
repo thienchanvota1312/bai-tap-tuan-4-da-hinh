23762ea23535ff0406c8c8cdc40c1497b4edf554
thienchanvota1312 <37357766+thienchanvota1312@users.noreply.github.com>
2020-08-13 20:26:27 +0700

    Create Circle.java

---
+++ Circle.java
@@ -0,0 +1,52 @@
+package Tuan4_2;
+
+public class Circle extends Shape{
+   
+    private double radius = 1.0;
+
+    public Circle(){}
+
+    public Circle(double radius){
+        this.radius =  radius;
+    }
+
+    public Circle(String color, boolean filed, double radius) {
+        super(color, filed);
+        this.radius = radius;
+    }
+
+    public double getRadius() {
+        return radius;
+    }
+
+    public void setRadius(double radius) {
+        this.radius = radius;
+    }
+
+    @Override
+    public String toString() {
+        return "Circle{" +
+                "radius=" + radius +
+                '}';
+    }
+
+    @Override
+    public double getArea () {
+        double radius = this.radius;
+        return  3.14*pow(radius,2);
+    }
+
+    @Override
+    public double getParameter() {
+        return 0;
+    }
+    public static void main(String[] args) {
+		    Circle c = new Circle("orange", false, 12);
+        String s = c.toString();
+        double a = c.getParameter();
+        double b = c.getArea();
+        System.out.println(s);
+        System.out.println(a);
+        System.out.println(b);
+	  }
+}
